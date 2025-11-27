a8797068073009d9dc7c71e6974c80f6e3c5a1dd
FranskyL1andy <230712405@students.uajy.ac.id>
2025-11-28 00:04:00 +0700

    +update_kost.php, +delete_kost.php

---
+++ php/CRUDs/kost/delete_kost.php
@@ -0,0 +1,8 @@
+<?php
+include "./php/auth/db_connection.php";
+
+$id = $_GET['kost_id'];
+
+mysqli_query($conn, "DELETE FROM kost WHERE kost_id = $id");
+header("Location: read_kost.php");
+?>
--- php/CRUDs/kost/update_kost.php
+++ php/CRUDs/kost/update_kost.php
@@ -1,14 +1,17 @@
-<?php
+<?php 
 include "./php/auth/db_connection.php";
+
+$id = $_GET['kost_id'];
+
+$data = mysqli_query($conn, "SELECT * FROM kost WHERE kost_id = $id");
+$row = mysqli_fetch_assoc($data);
 
 if(isset($_POST['submit'])){
     $nama_kost = $_POST['nama_kost'];
     $alamat = $_POST['alamat'];
     $deskripsi = $_POST['deskripsi'];
 
-    $query = "INSERT INTO kost(nama_kost, alamat, deskripsi) VALUES ('$nama_kost', '$alamat', '$deskripsi')";
-    mysqli_query($conn, $query);
-
+    mysqli_query($conn, "UPDATE kost SET nama_kost = $nama_kost, alamat = $alamat, deskripsi = $deskripsi WHERE kost_id = $id");
     header("Location: read_kost.php");
 }
 
@@ -16,9 +19,9 @@ if(isset($_POST['submit'])){
 
 <!DOCTYPE html>
 <html>
-    <head><title>Create Kost</title></head>
+    <head><title>Edit Kost</title></head>
     <body>
-        <h2>Create Kost</h2>
+        <h2>Edit Kost</h2>
         <?php if($message): ?> <p style="color:red;"><?= $message ?></p> <?php endif; ?>
 
         <form method="post" action="">
